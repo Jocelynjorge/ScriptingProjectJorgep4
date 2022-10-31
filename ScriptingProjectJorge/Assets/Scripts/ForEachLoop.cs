4ec5b30c56c47d22ae34e9cbf531846f474bb8ba
Jorge <719727@student.dusd.net>
2022-10-31 11:42:19 -0700

    Added the loop scene
    Scripted and added the loop scene

---
+++ ScriptingProjectJorge/Assets/Scripts/ForEachLoop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEachLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start ()
+    {
+        string[] strings = new string[3]
+
+        strings[0] = "First"    
+
+
+        
+            
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
