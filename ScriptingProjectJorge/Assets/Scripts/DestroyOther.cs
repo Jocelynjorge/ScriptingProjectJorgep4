50b05168da3c1206ef7cc94ad6817f20efa498bd
Jorge <719727@student.dusd.net>
2023-03-09 11:39:15 -0800

    SS

---
+++ ScriptingProjectJorge/Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }
+    }
+}
---
+++ ScriptingProjectJorge/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    //In this case ,result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+    //The represents the space between 3 and 5 which is 50% , so its 4
+
+    Vector3 from = new Vector3(1f, 2f, 3f);
+    Vector3 to = new Vector3(5f, 6f, 7f);
+
+    // Here result= (4,5,6)
+    Vector3 result = Vector3.Lerp(from, to, .75);
+
+    // Since 4 is 75% of the way from 1 and 5 and 5 is 75% of the way between 2 and 6 it shows that 75% is a pattern between each sequence 
+
+
+   
+    void Update()
+    {
+
+        Light.intensity = Mathf.Lerp(Light.intensity, 8f, 0.5f);
+
+    }
+
+    // This means that it will start at 0 then after one update it will go up to 4 then 6 then 7 and then 7.5 until it goes to 8
+    void Update ()
+    {
+        Light.intensity = Mathf.Lerp(Light.intensity, 8f, 0.5f* Time.deltaTime);
+
+    }
+    //The intensity would change every second instead of each frame 
+}
+    */
+    }
